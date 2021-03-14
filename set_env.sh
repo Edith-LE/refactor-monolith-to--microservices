c693fccae760c90970aabaf0ea06f9a8825dfb0e
Edith <edith.lopez@minu.mx>
2021-03-14 12:35:03 -0600

    divide an application into microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=udagramestdev
+export POSTGRES_PASSWORD=lasdemas.estrada
+export POSTGRES_HOST=udagramestdev.czpe5j7mvg6r.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-648329853730-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=sandia
 export URL=http://localhost:8100
